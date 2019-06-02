2504e1524f5a2f29fe74e0c49a668107d350ddd9
Leong Jia Meng <leongjiameng@outlook.com>
2019-06-02 09:00:30 +0800

    Thermometer

--- script.js
+++ script.js
@@ -17,8 +17,34 @@ window.onload = function () {
             .then(resp => resp.json())
             .then(jsonResp => {
                 document.getElementById('currentApiTemperature').innerHTML = `IoT Temperature: ${jsonResp[0]['temperature']} °`;
+                var gauge1=new LinearGauge({
+                    renderTo: 'gauge1',   
+                    width: 160,
+                    height: 600,
+                    units: '°C',
+                }).draw();
+                gauge1.value=jsonResp[0]['temperature'];
             });
     }
 
     getIoTTemperature();
 }
+function calendar(){
+    var day=['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+    var month=['January','February','March','April','May','June','July','August','September','October','November','December'];
+    var d=new Date();
+    setText('calendar-day', day[d.getDay()]);
+    setText('calendar-date', d.getDate());
+    setText('calendar-month-year', month[d.getMonth()]+' '+(1900+d.getYear()));
+};
+
+//this function will set the text value of 
+function setText(id, val){
+    if(val < 10){
+        val = '0' + val;    //add leading 0 if val < 10
+    }
+    document.getElementById(id).innerHTML = val;
+};
+
+//call calendar() when page load
+window.onload = calendar;
